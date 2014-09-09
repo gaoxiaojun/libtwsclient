71a89a5d49a1766fef8c6723f8f7776ba3014c4b
xiaojun gao <gaoxiaojun@gmail.com>
2014-09-09 20:30:42 +0800

    remove SIGINT & add default_logger

--- test/tws_demo.c
+++ test/tws_demo.c
@@ -2,17 +2,34 @@
 #include <string.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdarg.h>
 
 #include <twsclient/tws_client.h>
 
 extern void tws_debug(tws_client_t *client, int event_type, void *ud);
 
+static void debug_printf(FILE *stream, const char *label, const char *fmt, va_list ap)
+{
+  char fmtbuf[1024];
+  vsnprintf(fmtbuf, sizeof(fmtbuf), fmt, ap);
+  fprintf(stream, "%s: %s\n",label, fmtbuf);
+}
+
+void debug_logger(const char *fmt, ...) {
+  va_list ap;
+  va_start(ap, fmt);
+  debug_printf(stderr, "error", fmt, ap);
+  va_end(ap);
+}
+
+/*
 void sig_int_cb(uv_signal_t *handle, int signum)
 {
     printf("user interrupt\n");
     uv_signal_stop(handle);
     uv_stop(uv_default_loop());
 }
+*/
 
 int main(int argc, char **argv)
 {
@@ -31,9 +48,11 @@ int main(int argc, char **argv)
     if (!loop)
         return ENOMEM;
 
+    /*
     uv_signal_t sig_int;
     uv_signal_init(loop, &sig_int);
     uv_signal_start(&sig_int, sig_int_cb, SIGINT);
+    */
 
 #ifdef PLATFORM_POSIX
     signal(SIGPIPE, SIG_IGN);
@@ -44,6 +63,8 @@ int main(int argc, char **argv)
 
     if (!client)
         return ENOMEM;
+
+    client->logger = debug_logger;
 
     err = tws_client_connect(client, host, port);
     if (err)
